fc6bef86ee5d957e6560762e312acf9b781fa3b7
unknown <eulziisaikhan@miu.edu>
2021-12-13 11:57:07 -0600

    first commit

---
+++ src/main/java/gr0102/projectecommercewaa/ProjectEcommerceWaaApplication.java
@@ -0,0 +1,13 @@
+package gr0102.projectecommercewaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEcommerceWaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEcommerceWaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr0102/projectecommercewaa/ProjectEcommerceWaaApplicationTests.java
@@ -0,0 +1,13 @@
+package gr0102.projectecommercewaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEcommerceWaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
