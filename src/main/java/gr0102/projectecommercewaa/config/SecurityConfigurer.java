2a719db73169ce016e4a25791835f63b0a3eca04
unknown <eulziisaikhan@miu.edu>
2021-12-15 13:55:09 -0600

    fix cors configuration for authentication 2

--- src/main/java/gr0102/projectecommercewaa/config/SecurityConfigurer.java
+++ src/main/java/gr0102/projectecommercewaa/config/SecurityConfigurer.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import com.google.common.collect.ImmutableList;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
